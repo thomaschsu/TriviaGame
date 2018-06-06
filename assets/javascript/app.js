664178fa5492c68c1e0db718a86cd9f09533a73b
Tom Hsu <thomaschsu@gmail.com>
2018-06-05 23:49:30 -0400

    Modified css, js, html

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+// Variables
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+
+// Objects
+
+// Game starts with a page that has start button
+// Start button on click starts the game
+// One question is shown at a time
+// Time remaining is 30 seconds per question
+// One questions, 4 answer multiple choice
+// If unanswered, shows correct answer and goes to next question
+// Automatically moves to next question
+// Gif's on each question
+// At the end tells you how many correct, incorrect, and unanswered
+// Start over button on last page does not reload page, resets game
