7b22f0c3513324976e3bc31e679746c999db5e9e
Tom Hsu <thomaschsu@gmail.com>
2018-06-08 18:14:16 -0400

    Fixed comment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,7 @@
 // 1. Capture value of input
 // 2. If value = to ca then mark as correct, else mark as wrong
 // 3. Fix end game button
-// 4. Make conditional statement to count results
-// 5. Remove repeated code
+// 4. Remove repeated code
 
 // Variables
 var correct = 0;
